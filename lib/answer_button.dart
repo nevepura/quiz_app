14679d267f1d7c8e052416a3852269c31237d167
Federico Bicciato <federico.beach@gmail.com>
2024-05-30 23:50:06 +0200

    Added answer button; mapped first question with actual data

---
+++ lib/answer_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.all(7),
+        child: ElevatedButton(
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.black,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+          ),
+          child: Text(answerText),
+        ));
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,28 +15,38 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    var curQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Dummy question"),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Ans1"),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Ans2"),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Ans3"),
-          ),
-        ],
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: 40,
+          right: 40,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              curQuestion.text,
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 30),
+            ...curQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+            // An alternative way:
+            /*
+            for (var ans in curQuestion.answers)
+              AnswerButton(
+                answerText: ans,
+                onTap: () {},
+              )
+            */
+          ],
+        ),
       ),
     );
   }
